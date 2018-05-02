c65a9fc209e9449a97637da2de8257ba0f3e11f0
Jesse Whitham <jesse.whitham@gmail.com>
2018-05-02 16:50:48 +1200

    Verify Authentication
    * Use AWS parameter store for the verification token.
    * Use Slack go library - Slash command
    * Use lambda-go-api-proxy for converting APIGateway events to
      go net/http requests for library support

--- hello/main.go
+++ hello/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/awslabs/aws-lambda-go-api-proxy/core" // For converting APIGateway events to standard go http
+	"github.com/nlopes/slack" // Slack api library
 )
 
 // Response - Basic json response
@@ -14,9 +17,24 @@ type Response struct {
 
 // Handler - Handles Requests (Returns Echoed Message as Response)
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Println("Received body: ", request.Body)
+	// Build the APIGatewayProxy event into a http.request
+	accessor := core.RequestAccessor{}
+	r, nil := accessor.ProxyEventToHTTPRequest(request)
 
-	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
+	// Parse the SlashCommand from Slack
+	slashCommand, err := slack.SlashCommandParse(r)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: "No", StatusCode: 500}, nil
+	}
+
+	// Check the Verification token given from Slack matches our Bot Token
+	if !slashCommand.ValidateToken(os.Getenv("VERIFICATION_TOKEN")) {
+		return events.APIGatewayProxyResponse{Body: "No", StatusCode: 403}, nil
+	}
+
+	// Made it through.
+	return events.APIGatewayProxyResponse{Body: "Hi you passed", StatusCode: 200}, nil
 }
 
 func main() {
